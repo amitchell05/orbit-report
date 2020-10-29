7571dfefb02356a4be140c9375b74df512710be9
Angela Mitchell <anglarenmit@gmail.com>
2020-10-29 17:19:51 -0500

    Complete draft of bonus mission 2

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -9,33 +9,13 @@ import { Satellite } from '../satellite';
 export class OrbitCountsComponent implements OnInit {
   @Input() satellites: Satellite[];
 
-  types: string[];
-
   constructor() { }
 
   ngOnInit() {
   }
 
-  storeTypes(): boolean {
-    for (let i = 0; i < this.satellites.length; i++) {
-      if (!this.types.includes(this.satellites[i].type)) {
-        this.types.push(this.satellites[i].type);
-      }
-    }
-    console.log(this.types);
-    return true;
-  }
-
   typeCounter(currentType: string): number {
-    let count = 0;
-
-    for (let i = 0; i < this.types.length; i++) {
-      if (this.types[i].toLowerCase() === currentType.toLowerCase()) {
-        count++;
-      }
-    }
-
-    return count;
+    return this.satellites.filter(satellite => satellite.type === currentType).length;
   }
 
 }
